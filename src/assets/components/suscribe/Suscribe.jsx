dc6a8ccc079ece78d017c2a70dad5a412266c83f
Samuel Perez <martinsamuelpp@gmail.com>
2024-03-01 13:17:51 -0600

    creacion de data para tendencia y apartado suscribirse

---
+++ src/assets/components/suscribe/Suscribe.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Suscribe = () => {
+    return (
+        <section className='px-5 py-3 bg-grayCustom rounded-2xl w-11/12 mt-5'>
+            <h3 className='font-bold text-white text-xl'>Suscribete a Premium</h3>
+            <p className='text-white text-base py-2'>Suscríbete para desbloquear nuevas funciones y, si eres elegible, recibir un pago de cuota de ingresos por anuncios.</p>
+            <button className='bg-blue-500 text-customWhite font-medium text-base hover:bg-blue-600 transition duration-750 py-2 px-4 rounded-full'>Suscribete</button>
+        </section>
+    )
+}
+
+export default Suscribe
--- src/assets/layouts/sideBar/SideBar.jsx
+++ src/assets/layouts/sideBar/SideBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputSearch from '../../components/inputSearch/InputSearch'
+import Suscribe from '../../components/suscribe/Suscribe'
 
 const SideBar = () => {
     return (
         <article className=' hidden lg:block lg:w-1/3 bg-black pl-5 border-l-2 border-gray-800'>
             <InputSearch />
+            <Suscribe />
         </article>
     )
 }
